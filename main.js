6a1348dc3e7e415a5fc8879de9cb599c74da5e60
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2023-09-12 11:10:38 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,5 @@
 // 6kyu, 7kyu, 8kyu
 // KATA 1 7kyu #40
-// https://www.codewars.com/kata/573d11c48b97c0ad970002d4
-var regex2 = /https?:\/\/[\w\.]+\.(com|net)/gi
-
-// KATA 2 7kyu #41
-// https://www.codewars.com/kata/573e6831e3201f6a9b000971/train/javascript
-var regex=/\b(\w)(\w)?(\w)?\w?\3\2\1\b/g
-
-
-// KATA 3 7kyu #42
-// https://www.codewars.com/kata/573fb9223f9793e485000453 
-
-
-// KATA 4 7kyu 
-// 
-console.log();
-
-// KATA 5 7kyu 
-// 
-
-console.log();
-
-
-
-////THE LIST OF TRAINING
+////THE LIST OF TRAINING---in progress
 // https://www.codewars.com/kata/5722b3f0bd5583cf44001000/train/javascript  #40-42 left
 
